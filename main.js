627b313da5f2b1bd8d8905b99e09adb7c6c43e1f
cesc1802 <cesc.nguyen@vmodev.com>
2022-10-11 10:06:24 +0700

    update

--- main.js
+++ main.js
@@ -1,47 +1,13 @@
-const express = require("express");
-let http = require('http');
-let cors = require('cors')
-const bodyParser = require("body-parser");
+let http = require('http')
 let axios = require("axios")
-require('dotenv').config();
+require('dotenv').config()
 
-global.__base = __dirname + '/';
-global.__pathConfig = __base + 'config/';
-global.__pathRoutes = __base + 'routes/';
-global.__pathControllers = __base + 'controllers/';
-global.__pathServices = __base + 'services/';
+let sendEmail = require("./services/send_email")
 
-const initWebRoutes = require(__pathRoutes + "web");
-let sendEmail = require(__pathServices + "send_email")
-
-let app = express();
-//cấp phép truy cập api
-
-app.use(cors())
-// use midleware bodyparser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", initWebRoutes());
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-app.use(function(err, req, res, next) {
-    res.locals.userInfo = '';
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('404'); ///khi không tìm được trang sẽ trả về trang báo lỗi có thông báo lỗi đầy đủ
-    // res.render('error', { title: 'errorPage' });
-});
-
-let port = process.env.PORT || 6969; // ||hoặc
-// PORT === undefined thì gán vào 6969
-let server = http.createServer(app);
+let port = process.env.PORT || 6969
+let server = http.createServer(app)
 server.listen(port, () => {
-    console.log(`app is running at port: http://localhost:${port}`);
+    console.log(`app is running at port: http://localhost:${port}`)
 });
 
 axios.get(
@@ -61,5 +27,30 @@ setInterval(()=>{
         console.log(err)
     })
     
-}, 86400000)
+}, 43200000)
 // 86400
+
+function app(req, res){
+    switch(req.method){
+        case "GET":
+            switch(req.url){
+                case "/":
+                    axios.get(
+                        'https://api.ipify.org?format=json',
+                    ).then(result =>{
+                        res.writeHead(200, {'Content-Type':'application/json' })
+                        res.end(result.data.ip)
+                    }).catch(err=>{
+                        res.writeHead(500, {'Content-Type':'application/json' })
+                        res.end(err)
+                    })
+                    break
+                default:
+                    res.writeHead(404, {'Content-Type':'application/json' })
+                    res.end("404 not found!!!")
+            }
+            break
+        case "POST":
+            break
+    }
+}
